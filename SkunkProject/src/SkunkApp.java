af9201fe1913c3eda10ae101836b3984be391dc8
michellecomtech <michellecomtech@gmail.com>
2019-10-04 07:21:17 -0500

    added .gitkeep so test folder is kept

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -6,6 +6,9 @@ public class SkunkApp
 	public static void main(String[] args)
 	{
 		StdOut.println("Welcome to 635 Skunk!");
+		Dice dice1 = new Dice();
+		dice1.roll();
+		StdOut.println(dice1);
 
 	}
 
